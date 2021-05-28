98daa208d839c065b166f1f1ce38b86af3bea877
nrp155 <84077310+nrp155@users.noreply.github.com>
2021-05-28 22:02:46 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,97 @@
+# [Clone Bot](https://github.com/m4mallu/clonebot)
+
+#### A simple telegram bot can clone medias from any chat to your private chat -cmd based 😍 
+### 👉 cmd based design 👈
+#### CLONE BOT MAIN PAGE&nbsp;&nbsp;<a href="https://github.com/m4mallu/clonebot"><img alt="Renjith Mangal" src ="https://www.seekpng.com/png/full/97-971723_download-click-here-button-png-click-here-button.png" width="100" /></a>
+<br>
+
+### 🤘 𝙋𝙧𝙞𝙫𝙖𝙩𝙚 & 𝙋𝙪𝙗𝙡𝙞𝙘 𝙘𝙝𝙖𝙩𝙨 𝙨𝙪𝙥𝙥𝙤𝙧𝙩𝙚𝙙 - 𝘼𝙣𝙮𝙤𝙣𝙚 𝙘𝙖𝙣 𝙘𝙡𝙤𝙣𝙚 𝙢𝙚𝙙𝙞𝙖𝙨 𝙛𝙧𝙤𝙢 𝙖𝙣𝙮𝙬𝙝𝙚𝙧𝙚 𝙩𝙤 𝙮𝙤𝙪𝙧 𝙥𝙚𝙧𝙨𝙤𝙣𝙖𝙡 𝙘𝙝𝙖𝙩 🤘
+
+### ░▒▓ Highlights of this Bot ▓▒░
+💢 **String session user doesn't need to be an admin of the Source chat**<br>
+💢 **String session user need to be an admin of the Destination chat**<br>
+💢 **Bot doesn't need to be added in the Source / Destination chats** 😍
+
+### ░▒▓ Keep in mind ▓▒░
+🚫 𝘾𝙧𝙚𝙖𝙩𝙚 𝙖 𝙨𝙩𝙧𝙞𝙣𝙜 𝙨𝙚𝙨𝙨𝙞𝙤𝙣 𝙛𝙧𝙤𝙢 𝙖 𝙪𝙨𝙚𝙧 𝙖𝙘𝙘𝙤𝙪𝙣𝙩 ( 𝙙𝙤𝙣'𝙩 𝙜𝙚𝙣𝙚𝙧𝙖𝙩𝙚 - 𝙪𝙨𝙚 𝙗𝙤𝙩 𝙨𝙚𝙨𝙨𝙞𝙤𝙣 ) <br>
+👉 𝘾𝙡𝙞𝙘𝙠 [𝙃𝙀𝙍𝙀](https://repl.it/@ayrahikari/pyrogram-session-maker) 𝙩𝙤 𝙜𝙚𝙣𝙚𝙧𝙖𝙩𝙚 𝙖 𝙪𝙨𝙚𝙧 𝙨𝙩𝙧𝙞𝙣𝙜 𝙨𝙚𝙨𝙨𝙞𝙤𝙣 <br>
+🚫 𝙎𝙩𝙧𝙞𝙣𝙜 𝙨𝙚𝙨𝙨𝙞𝙤𝙣 𝙪𝙨𝙚𝙧 𝙨𝙝𝙤𝙪𝙡𝙙 𝙣𝙤𝙩 𝙗𝙚 𝙗𝙖𝙣𝙣𝙚𝙙 𝙞𝙣 𝙩𝙝𝙚 𝙨𝙤𝙪𝙧𝙘𝙚 𝙘𝙝𝙖𝙩
+
+### ░▒▓ Mandatory Variables  ▓▒░
+```
+TG_BOT_TOKEN    - Get from @BotFather
+APP_ID          - Get from my.telegram.org
+API_HASH        - Get from my.telegram.org
+TG_USER_SESSION - Run any userbot session maker (Mentioned above)
+```
+
+### ░▒▓ @BotFather Commands ▓▒░
+**Should Be run in Bot**
+
+```
+/help - To know how to use the bot
+/source - Set source chat Id ( /source -1001234567890 )
+/destination - Set destination chat Id ( /destination -1009876543210)
+/view - To view the current stored chat configuration
+/delconfig - To clear the stored chat configuration
+/clone - Clone medias from source to destination chat
+
+```
+### ░▒▓ Deploy Easy Way: ▓▒░
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/m4mallu/clonebot-cmd)
+
+### ░▒▓ Deploy Legendary Way: ▓▒░
+
+Create **config.py** with variables as given below [Refer sample.config](https://github.com/m4mallu/clonebot-cmd/blob/master/sample_config.py)
+
+```
+class Config(object):
+    TG_BOT_TOKEN = "134448596:AAEIyo3EBVCN3qdd3TfrmQUxoI-eZVGvmI"
+    APP_ID = int(123635)
+    API_HASH = "1a417dd4fdf3ead2819ff35641daa16b"
+    TG_USER_SESSION = "BQDGRUC0_qw2GVQ2gpLFaXOt0mrWg16cBZPATQvR8KThDzi-NRE1I9DB......"
+```
+𝘾𝙡𝙞𝙘𝙠 [𝙃𝙀𝙍𝙀](https://repl.it/@ayrahikari/pyrogram-session-maker) 𝙩𝙤 𝙜𝙚𝙣𝙚𝙧𝙖𝙩𝙚 𝙖 𝙪𝙨𝙚𝙧 𝙨𝙩𝙧𝙞𝙣𝙜 𝙨𝙚𝙨𝙨𝙞𝙤𝙣 <br>
+
+#### ░▒▓ Run the following: ▓▒░
+
+```
+virtualenv -p python3 venv
+. ./venv/bin/activate
+pip3 install -r requirements.txt
+python3 main.py
+```
+
+### ░▒▓ Bot working ▓▒░
+
+<p align="left">
+  <a href="https://telegra.ph/file/3f3e9045efbafbbff1bbc.mp4" type="video/mp4">
+    <img alt="Renjith Mangal" src ="https://static.thenounproject.com/png/2862643-200.png" width="150" />
+  </a>
+</p>
+
+## ░▒▓ LICENSE ▓▒░
+<p align="left">
+  <a href="https://choosealicense.com/licenses/gpl-3.0">
+    <img alt="GPL3" src ="https://e-tinet.com/wp-content/uploads/2017/02/logo-gnu-linux-GPL-2.png" width="104.75" height="32"/>
+  </a>
+</p>
+<br>
+
+## ░▒▓ Credits ▓▒░
+<p align="left">
+  <a href="https://github.com/pyrogram/pyrogram">
+    <img alt="Pyrogram" src ="https://docs.pyrogram.org/_static/pyrogram.png" width="104.75" height="32"/>
+  </a>
+</p>
+<br>
+
+## ░▒▓ Finally ▓▒░
+<p align="left">
+  <a href="https://t.me/space4renjith">
+    <img alt="Renjith Mangal" src ="https://telegra.ph/file/1001b7b08b0e9c266f780.png" width="150" />
+  </a>
+</p>
+
+[𝐃𝐢𝐬𝐜𝐥𝐚𝐢𝐦𝐛𝐞𝐫:](https://telegra.ph/Disclaimer---Clonebot-05-25)
---
+++ plugins/clone.py
@@ -0,0 +1,118 @@
+#----------------------------------- https://github.com/m4mallu/clonebot --------------------------------------------#
+import time
+import pytz
+import datetime
+import asyncio
+from bot import Bot
+from presets import Presets
+from pyrogram import filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from init import source_chat, destination_chat
+from pyrogram.errors import FloodWait
+
+bot_start_time = time.time()
+
+@Bot.on_message(filters.private & filters.command('clone'))
+async def clone_medias(client: Bot, message: Message):
+    ID = int(message.from_user.id)
+
+    clone_start_time = time.time()
+    current_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%I:%M:%S %p')
+
+    doc_files = 0
+    video_files = 0
+    audio_files = 0
+
+    if ID in source_chat and destination_chat:
+        user_bot_me = await client.USER.get_me()
+        msg = await client.send_message(
+            chat_id=message.chat.id,
+            text=Presets.INITIAL_MESSAGE_TEXT,
+            disable_notification=True
+        )
+        msg1 = await client.send_message(
+            chat_id=message.chat.id,
+            text=Presets.WAIT_MSG
+        )
+        # Checking string session user is in the given source chat id as self
+        try:
+            await client.USER.get_chat_member(chat_id=source_chat[ID], user_id=user_bot_me.id)
+        except Exception:
+            await msg1.delete()
+            await msg.edit(
+                text=Presets.IN_CORRECT_PERMISSIONS_MESSAGE,
+            )
+            source_chat.pop(ID)
+            destination_chat.pop(ID)
+            return
+        try:
+            await msg1.edit(
+                text=Presets.CLOSE_BTN_TXT,
+                reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton(text="🌀 CANCEL 🌀", callback_data="stop_clone")]]
+                    )
+            )
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+        except Exception:
+            pass
+        async for user_message in client.USER.iter_history(chat_id=source_chat[ID], reverse=True):
+            messages = await client.USER.get_messages(
+                source_chat[ID],
+                user_message.message_id,
+                replies=0,
+            )
+            time_taken = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - clone_start_time))
+            uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time))
+            update = datetime.datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%I:%M:%S %p')
+            for file_type in tuple(Presets.FILE_TYPES):
+                media = getattr(messages, file_type, None)
+                if media is not None:
+                    if file_type == 'document':
+                        doc_files += 1
+                    elif file_type == 'video':
+                        video_files += 1
+                    elif file_type == 'audio':
+                        audio_files += 1
+                    else:
+                        pass
+                    try:
+                        await msg.edit(
+                            text=Presets.MESSAGE_COUNT.format(
+                                doc_files,
+                                video_files,
+                                audio_files,
+                                time_taken,
+                                uptime,
+                                current_time,
+                                update
+                            )
+                        )
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)
+                    except Exception:
+                        pass
+                    try:
+                        await client.USER.copy_message(
+                            chat_id=destination_chat[ID],
+                            from_chat_id=source_chat[ID],
+                            caption=messages.caption,
+                            message_id=messages.message_id,
+                            disable_notification=True
+                        )
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)
+                    except Exception:
+                        # Clone error due to string session user is not an admin of the given destination chat !
+                        await msg1.delete()
+                        await msg.edit(Presets.COPY_ERROR_TEXT)
+                        source_chat.pop(ID)
+                        destination_chat.pop(ID)
+                        return
+    else:
+        await message.reply_text(Presets.NOT_CONFIGURED)
+        return
+    await msg1.edit(Presets.FINISHED_TEXT)
+    source_chat.pop(ID)
+    destination_chat.pop(ID)
+    return
